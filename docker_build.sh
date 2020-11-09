f537558e409e5a525b2857ed44995a5bd6f31de4
Kai Weeks <kai.weeks@gmail.com>
2020-11-09 14:47:55 +0000

    Build process segregation and generified

--- docker_build.sh
+++ docker_build.sh
@@ -2,7 +2,7 @@
 # Gradle build in Docker with persistent Gradle cache.
 #
 # author: Weleoka <weleoka@mailfence.com>
-# edited: 2020-11-08
+# edited: 2020-11-09
 #
 # Creates a persistent gradle cache for the build container here in the project /build dir.
 # Because Gradle is "installed" for a normal user in the official Gradle docker images we have to work around that.
@@ -18,15 +18,23 @@
 set -euo pipefail
 script="`realpath "$0"`"
 project_dir="`dirname "$script"`"
-docker_gradle_image="gradle:6.7.0-jdk11"
-# gradle:6.7.0-jdk11 is gradle@sha256:33f85a6c2d3fd5f81987f66db3a3e379e562587e20b5ae404d8c4f7f62f37fb3
+# Gradle Build Image name
+gbi_name="my-gbi"
+# Gradle dependencies cache:
+gd_cache_dir="${project_dir}"/build/docker_build_cache
+# Artifacts saved to host:
+artifacts_dir="${project_dir}/my_artifacts"
+# This is the problem... the Gradle process builds the Jar to an explicit dir depending on java module:
+container_artifacts_dir="/home/gradle/service/build/libs"
 
 echo
-echo "== Building Gradle project: ${project_dir} in Docker container (${docker_gradle_image})."
+echo "== Building Gradle project: ${project_dir} in a Docker container."
 
-echo "> Create persistent build cache directory."
-mkdir -p "$project_dir"/build/docker_build_cache
+echo "> Create persistent build cache directory on host."
+mkdir -p "$gd_cache_dir"
 
+echo "> Create artifacts storage directory on host."
+mkdir -p "$artifacts_dir"
 
 # Note added this as removing classes in source code was not reflected
 # in the extracted archive contents after build. Due to class autoloading
@@ -36,20 +44,29 @@ echo "> (disabled) Removing build/application and build/classes folder to ensure
 #rm -r "${project_dir}/build/application"
 #rm -r "${project_dir}/build/classes"
 
+echo "> Prepare the build image..."
+#docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) -f Dockerfile_build -t my-gbi .
+# similar to:
+docker build \
+--build-arg UID=$(id -u) \
+--build-arg GID=$(id -g) \
+-f Dockerfile_build \
+-t "$gbi_name" .
 
-echo "> Start the build process..."
-# Choose here if you want to do manual bash commands or the provided ones.
-docker run --name gradle_builder --rm \
--v "$project_dir":/home/gradle \
--v "$project_dir"/build/docker_build_cache:/home/gradle/.gradle/caches \
--w /home/gradle \
-"$docker_gradle_image" \
+echo "> Build artifacts in build container..."
+#docker run -it --name gradle_builder --rm -v $(pwd)/build/docker_build_cache:/home/gradle/.gradle/caches my-gbi:latest bash
+# similar to:
+docker run --rm \
+--name gradle_builder \
+-v "$gd_cache_dir":/home/gradle/.gradle/caches \
+-v "$artifacts_dir":"$container_artifacts_dir" \
+"$gbi_name" \
 /bin/bash -c "gradle --quiet --no-daemon build"
+# -v "$project_dir":/home/gradle \
 #/bin/bash -c "gradle --quiet --no-daemon bootJar"
 #/bin/bash
 
-
-# Todo: multilayer jars so run-container can have optimised docker caching.
+# Todo: multilayer jars, so run-container can have optimised docker caching.
 #/bin/bash -c "gradle --quiet --no-daemon bootJar && cd build && java -Djarmode=layertools -jar ./libs/*.jar extract"
 #/bin/bash -c "gradle --quiet --no-daemon bootJar && cd service/build && java -Djarmode=layertools -jar ./libs/*.jar extract"
 # todo: the extracting of multilayer jar is not very neat as we have to cd to ./build to extract to build.
@@ -57,8 +74,7 @@ docker run --name gradle_builder --rm \
 # todo: Q: the --no-daemon flag for gradle? Might be important for parallel running,
 #   as per: https://dev.to/markomannux/gradle-daemon-with-multi-module-spring-project-3nog
 
-
-echo "> Remove *.lock in Gradle persistent cache for next time running."
+echo "> Remove *.lock files in persistent build cache."
 rm -r "$project_dir"/build/docker_build_cache/modules-2/*.lock 2> /dev/null
 
 
