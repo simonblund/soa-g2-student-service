e18635149f3de30432115d228211cda7e5df6ff9
Simon Blomsterlund <simon@simonblund.com>
2020-11-20 19:41:08 +0200

    Added canvas api

---
+++ api/src/main/java/com/g2/studentservice/api/mock/canvas/Grade.java
@@ -0,0 +1,5 @@
+package com.g2.studentservice.api.mock.canvas;
+
+public enum Grade {
+    U, G, VG
+}
---
+++ api/src/main/java/com/g2/studentservice/api/mock/canvas/ResultResponse.java
@@ -0,0 +1,20 @@
+package com.g2.studentservice.api.mock.canvas;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResultResponse {
+    private String ssn;
+    private String courseCode;
+    private String module;
+    private String date;
+    private Grade grade;
+}
