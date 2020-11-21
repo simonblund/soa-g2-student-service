c82c79da8d95ed2732e8099787b89bd415b3fa1e
Simon Blomsterlund <simon@simonblund.com>
2020-11-21 21:01:41 +0200

    adjust mocurlpats

--- api/src/main/java/com/g2/studentservice/api/mock/MockUrlPaths.java
+++ api/src/main/java/com/g2/studentservice/api/mock/MockUrlPaths.java
@@ -1,7 +1,7 @@
 package com.g2.studentservice.api.mock;
 
 public class MockUrlPaths {
-    public static final String EPOKMODUL = "/modul";
+    public static final String EPOKMODUL = "/modules";
     public static final String CANVAS = "/results";
-    public static final String STUDENTITS = "/student";
+    public static final String STUDENTITS = "/students";
 }
