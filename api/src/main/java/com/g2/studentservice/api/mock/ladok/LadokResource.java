8d16b1187ab351dac0200704291a9aeccf4a9359
Simon Blomsterlund <simon@simonblund.com>
2020-11-22 00:39:47 +0200

    adjust mocurlpats

--- api/src/main/java/com/g2/studentservice/api/mock/ladok/LadokResource.java
+++ api/src/main/java/com/g2/studentservice/api/mock/ladok/LadokResource.java
@@ -2,10 +2,15 @@ package com.g2.studentservice.api.mock.ladok;
 
 import com.g2.studentservice.api.mock.MockUrlPaths;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 public interface LadokResource {
     @RequestMapping(method = RequestMethod.GET, path = MockUrlPaths.LADOK)
     ResponseEntity<ResultResponse> getResult();
+
+    @RequestMapping(method = RequestMethod.PATCH, path = MockUrlPaths.LADOK+"{id}")
+    ResponseEntity<ResultResponse> setResult(@PathVariable String id, @RequestBody ResultResponse body);
 }
