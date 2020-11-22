46c9eb3f532c94161b83335b69443f81fff58762
Simon Blomsterlund <simon@simonblund.com>
2020-11-22 14:46:20 +0200

    Fix get one epokmoduel

--- api/src/main/java/com/g2/studentservice/api/mock/epok/EpokModulResource.java
+++ api/src/main/java/com/g2/studentservice/api/mock/epok/EpokModulResource.java
@@ -2,6 +2,7 @@ package com.g2.studentservice.api.mock.epok;
 
 import com.g2.studentservice.api.mock.MockUrlPaths;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,4 +11,7 @@ import java.util.List;
 public interface EpokModulResource {
     @RequestMapping(method = RequestMethod.GET, path = MockUrlPaths.EPOKMODUL)
     ResponseEntity<List<ModulResponse>> getAll();
+
+    @RequestMapping(method = RequestMethod.GET, path = MockUrlPaths.EPOKMODUL+"?code={module}")
+    ResponseEntity<ModulResponse> getOne(@PathVariable String module);
 }
