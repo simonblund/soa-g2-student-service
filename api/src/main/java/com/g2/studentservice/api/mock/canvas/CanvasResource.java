b4b6591a0a9d4c6d5a3149a182194a6623687b8f
Simon Blomsterlund <simon@simonblund.com>
2020-11-24 23:09:03 +0200

    Get canvas endpoint to work better

--- api/src/main/java/com/g2/studentservice/api/mock/canvas/CanvasResource.java
+++ api/src/main/java/com/g2/studentservice/api/mock/canvas/CanvasResource.java
@@ -3,6 +3,7 @@ package com.g2.studentservice.api.mock.canvas;
 import com.g2.studentservice.api.mock.MockUrlPaths;
 import com.g2.studentservice.api.mock.ladok.ResultResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,7 +14,7 @@ public interface CanvasResource {
     ResponseEntity<List<AssignmentResponse>> getAssignments();
 
     @RequestMapping(method = RequestMethod.GET, path = MockUrlPaths.CANVAS+"?assignment_id={assignmentId}")
-    ResponseEntity<AssignmentResponse> getAssignment(String assignmentId);
+    ResponseEntity<AssignmentResponse> getAssignment(@PathVariable String assignmentId);
 
     @RequestMapping(method = RequestMethod.GET, path = MockUrlPaths.CANVAS+"?module_id={moduleCode}")
     ResponseEntity<List<AssignmentResponse>> getAssignmentsForExamination(String moduleCode);
